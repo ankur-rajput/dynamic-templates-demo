ea81ddc3f2dd9557ed508a328932f3361b2e9098
Ankur Rajput <ankur@tothenew.com>
2019-04-20 02:37:40 +0530

    demo exercises

--- core/src/main/java/edu/ttn/templates/demo/core/models/HeaderModel.java
+++ core/src/main/java/edu/ttn/templates/demo/core/models/HeaderModel.java
@@ -7,7 +7,6 @@ import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.designer.Style;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
@@ -17,8 +16,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 @Model(
@@ -48,75 +45,7 @@ public class HeaderModel {
     @ScriptVariable
     private Page currentPage;
 
-    @ScriptVariable
-    private ResourceResolver resolver;
-
-    private Map<String, String> languages = new HashMap<>();
-
-    private String currentLanguage;
-
-    private String pagePath;
-
-    private String pageLocale;
-
     private Resource headerResource;
 
-    @PostConstruct
-    protected void init() {
-
-        //populate header properties
-        setHeaderResource();
-        if (Objects.nonNull(headerResource)) {
-            ValueMap headerValueMap = headerResource.getValueMap();
-            logoURL = headerValueMap.get(LOGO_URL_PROPERTY_NAME, String.class);
-            siteName = headerValueMap.get(SITE_NAME_PROPERTY_NAME, String.class);
-        }
-    }
-
-    /**
-     * Look beneath current resource for Header resource
-     * if not defined, iterate up content tree to find header resource
-     */
-    private void setHeaderResource() {
-        String relativeHeaderPath = currentStyle.get(REL_PATH_PROPERTY_NAME, DEFAULT_REL_PATH);
-        //target page where navigation is defined
-        Page targetPage = currentPage;
-        headerResource = targetPage.getContentResource(relativeHeaderPath);
-
-        //iterate until we find a header resource that is not null or finish traversing the hierarchy
-        while (isEmptyHeader(headerResource) && targetPage != null) {
-            targetPage = targetPage.getParent();
-            if (targetPage != null) {
-                headerResource = targetPage.getContentResource(relativeHeaderPath);
-            }
-        }
-
-    }
-
-    public String getLogoURL() {
-        return logoURL;
-    }
-
-    public String getSiteName() {
-        return siteName;
-    }
-
-    /**
-     * Return true if the header resource is null or not populated
-     *
-     * @param headerResource
-     * @return
-     */
-    private boolean isEmptyHeader(Resource headerResource) {
-        if (headerResource != null) {
-
-            final String rootPathValue = headerResource.getValueMap().get(SITE_NAME_PROPERTY_NAME, String.class);
-
-            if (rootPathValue != null && rootPathValue != "") {
-                return false;
-            }
-        }
-        return true;
-    }
 
 }
